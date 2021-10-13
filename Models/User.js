8673d02c8225ba05e47a3c1e9291d1f79c10eb86
ShahidBangash <shahidbangashsi94@gmail.com>
2021-10-13 09:05:34 +0500

    removed User Schema , It was no longer needed

--- Models/User.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-var UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  gender: { type: String, enum: ["Male", "Female"] },
-  password: String,
-});
-module.exports = mongoose.model("Users", UserSchema);
