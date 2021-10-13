cac1cea25132b4c5b8f62ae34bb10baa477a0564
ShahidBangash <shahidbangashsi94@gmail.com>
2021-10-13 09:05:01 +0500

    removed Garden schema

--- Models/garden.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var GardenSchema = new mongoose.Schema({
-  name: String,
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
-  length: Number,
-  width: Number,
-});
-
-module.exports = mongoose.model("Garden", GardenSchema);
--- Models/plants.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var PlantSchema = new mongoose.Schema({
-  name: String,
-  belongsTo: { type: mongoose.Schema.Types.ObjectId, ref: "Garden" },
-  date: Date,
-  description: String,
-});
-
-module.exports = mongoose.model("Garden", PlantSchema);
